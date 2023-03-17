0778eb6a253689debb8d96089536aa71fca446d2
KimYura2 <ururur68@gmail.com>
2023-03-17 13:56:06 +0900

    order페이지 수정 - input창에 입력값 없을 시, label css 다시 제자리로. + ajax : application/json로 보내기

--- public/js/order.js
+++ public/js/order.js
@@ -1,14 +1,19 @@
-$(document).ready(function() {
+$(function() {
+    // 3) 주문서정보 입력후 벗어날(focusout)때, 해당 input칸이 빈칸이 아니면 해당 주문서정보를 감싸고있는 <label></label>에 has-value클래스 추가
     $('.or-form-component input.text, .or-form-component textarea.text').on('focusout', function(e) {
-        if( this.value != '' ) $(this).parent().addClass('has-value');
-        else $(this).parent().removeClass('has-value');
+        if( this.value != '' ) // Order - 주문하시는 분/연락처... 등 "주문서 정보"칸이 빈칸이 아니면
+            $(this).parent().addClass('has-value'); // 해당 주문서정보를 감싸고있는 <label></label>에 has-value클래스 추가
+        else 
+            $(this).parent().removeClass('has-value');
     });
 
-    $('.or-form-component .label-text').on('click', function(e) {
-        $(this).closest('label').find('.text').focus();
+    // 1) 주문하시는분 label을 클릭시, 해당 input에 포커스 주기(포커스를 얻는것 : input태그를 마우스로 클릭하여 "입력상태"로 만든것)
+    $('.or-form-component .label-text').on('click', function(e) { // label-text : <label>주문하시는 분</label>을 클릭하면
+        $(this).closest('label').find('.text').focus(); // <input type="text" class="text" name="userId"...>에 포커스를 줌 (포커스를 얻는것 : input태그를 마우스로 클릭하여 "입력상태"로 만든것)
     });
 
-    $('.or-form-component input.text, .or-form-component textarea.text').focusin(function(){
+    // 2) input에 포커스가 들어갔을 때. 주문하시는분 label css변경(라벨 글씨 작아지며 올라감)
+    $('.or-form-component input.text, .or-form-component textarea.text').focusin(function(){ // focusin() : .focus가 들어갔을때 이벤트 발생
         $(this).next().css({    
             fontSize : '0.6em',
             height : '25px',
@@ -18,7 +23,21 @@ $(document).ready(function() {
         })
     })
 
-    $('#member_post').focusin(function(){
+    // input창에 입력값 없을 시, label css 다시 제자리로.
+    $('.or-form-component input.text, .or-form-component textarea.text').focusout(function(){ // focusin() : .focus가 들어갔을때 이벤트 발생
+        if( this.value == '' ){
+            $(this).next().css({    
+                fontSize : '0.8em',
+                height : '40px',
+                lineHeight : '40px',
+                top:'35px',
+                transition : 'all 0.4s'  ,
+            })
+        }
+    })
+
+    // 우편번호 항목에 포커스인됐을때
+    $('#member_post').focusin(function(){ 
         $(this).parent().parent().next().children().children().css({
             fontSize : '0.6em',
             height : '25px',
@@ -28,6 +47,7 @@ $(document).ready(function() {
         })
     })
     
+    // 주소 항목에 포커스인 됐을때
     $('#member_addr').focusin(function(){
         $(this).parent().parent().prev().children().children().css({
             fontSize : '0.6em',
@@ -41,15 +61,18 @@ $(document).ready(function() {
 });
 
 
-setTimeout(function() {
-    $('.or-form-component input.text').each(function(e) {
-        if( this.value != '' ) $(this).parent().addClass('has-value');
-        else $(this).parent().removeClass('has-value');
-    });
-}, 150);
+// 주문서정보 input칸이 빈칸이 아닐때 ,해당 주문서정보를 감싸고있는 <label></label>에 has-value클래스 추가
+// setTimeout(function() {
+//     $('.or-form-component input.text').each(function(e) {
+//         if( this.value != '' )
+//             $(this).parent().addClass('has-value');
+//         else 
+//             $(this).parent().removeClass('has-value');
+//     });
+// }, 300);
 
 
-
+// 다음 주소 API : join-주소 + order-주소
 function findAddr(){
 	new daum.Postcode({
         oncomplete: function(data) {
@@ -73,10 +96,8 @@ function findAddr(){
     }).open();
 }
 
-
+// 결제버튼 클릭시 실행
 function orderBuy() {
-
-    
     var userName = $("input[name='userName']").val();
     var userPhoneNum = $("input[name='userPhoneNum']").val();
     var userEmail = $("input[name='userEmail']").val();
@@ -94,6 +115,7 @@ function orderBuy() {
 
     var orderCheck = $("input[name='orderCheck']");
 
+    // 주문서 입력정보 data에 담기
     var data = {
         'userName': userName,
         'userPhoneNum': userPhoneNum,
@@ -109,8 +131,7 @@ function orderBuy() {
         'totalSettlePrice': totalSettlePrice
     };
 
-    
-
+    // 주문서 필수입력정보 유효성 검사
     if(receiverName==""){
             alert("성명을 입력하세요.");
             receiverName.focus();
@@ -141,13 +162,15 @@ function orderBuy() {
         orderCheck.focus();
         return false;
     }
-    
 
-    
+    var datajson = JSON.stringify(data);
+  
+    //서버에 ajax 요청
     $.ajax({
       type: 'POST', 
       url: '/order/complete', //서버측에서 가져올 페이지
-      data: data, // {'userName': userName}처럼 json형태의 데이터를 서버로 전달. 
+      data: datajson, // {'userName': userName}처럼 "json형태의 데이터"를 서버로 전달. (요청과 함께 서버로 데이터를 전송 할 "string" 또는 map)
+      contentType:  "application/json; charset=utf-8",
       success: function(response){ // 데이터 통신이 정상적으로 이루어졌을 때 호출하기.
         if(response) {
           if(confirm('결제가 완료되었습니다. ')) {
@@ -155,8 +178,6 @@ function orderBuy() {
             console.log(response.message);
             alert(response.testorder);
             $('#post_output').html(response.testorder);
-
-            // location.href="/ordernum";
           }else{
             alert("전송된 값 없음")
           }
